4e41992f4bf2e96def99f88e756c8ae75e6c6093
Alok Jadhav <ajadhav@qualys.com>
2018-12-07 10:42:39 +0530

    user01: added file

---
+++ configFile.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ajadhav
+#
+# Created:     06/12/2018
+# Copyright:   (c) ajadhav 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+## Input-01
+#
+# Specify Data Base which need to refer
+
+dbName = "RHEL_6.0_UDC.db"
+
+
+## Input-02
+#
+# Specify List of DB table for which DPID count need to be generated
+
+dbTableNameList = [ "DirSearchCheckData", "DirIntegrityCheckData"]
+
+
